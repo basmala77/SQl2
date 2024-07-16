dc100acaed45505f92040fbe0a3244c5d8514ff1
basmala77 <120977520+basmala77@users.noreply.github.com>
2024-07-16 16:28:56 +0300

    Update1

---
+++ sqltask2.sql
@@ -0,0 +1,34 @@
+----------------Join Tables------------------------
+SELECT e.Name, d.DepartmentName FROM Employees e JOIN Departments d ON e.DepartmentID = d.DepartmentID;
+
+----------------Aggregate Functions-------------------
+
+
+----------------Grouping and Ordering-----------------
+
+
+----------------Selecting Top Records------------------
+
+
+----------------Nested Queries-------------------------
+
+
+----------------Creating and Modifying Views---------------
+
+
+----------------Implementing Stored Procedures---------------
+
+
+----------------Utilizing Built-in Functions------------------
+
+
+----------------Working with Temporary Data-------------------
+
+
+----------------Handling Exceptions--------------------
+
+
+----------------Implementing Trigger-------------------
+
+----------------Creating User-defined Functions-------------------
+
