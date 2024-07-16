4fcbc9a07a90e71eb0d8a715ea4690b2bb2e0922
basmala77 <120977520+basmala77@users.noreply.github.com>
2024-07-17 01:26:11 +0300

    Update sqltask2.sql

--- sqltask2.sql
+++ sqltask2.sql
@@ -5,19 +5,25 @@ SELECT e.Name, d.DepartmentName FROM Employees e JOIN Departments d ON e.Departm
 SELECT AVG(Salary) AS AverageSalary FROM Employees;
 
 ----------------Grouping and Ordering-----------------
-
+SELECT Dep_name, COUNT(*) AS NUM_Employee FROM  Employees GROUP BY Dep_name ORDER BY NUM_Employee;
 
 ----------------Selecting Top Records------------------
-
+SELECT TOP 10 employee_id,salary FROM Employees ORDER BY salary DESC;
 
 ----------------Nested Queries-------------------------
+SELECT* FROM Employees WHERE Dip_id IN (SELECT Dep_id FROM Departments WHERE region = 'East');
 
 
 ----------------Creating and Modifying Views---------------
-
+CREATE VIEW ActiveEmployees AS 
+  SELECT * FROM  Employees WHERE status = 'Active';
 
 ----------------Implementing Stored Procedures---------------
-
+CREATE PROCEDURE EmployeeDetails
+AS
+BEGIN
+    SELECT * FROM Employees;
+END;
 
 ----------------Utilizing Built-in Functions------------------
 
